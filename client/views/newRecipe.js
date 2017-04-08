ae50d889b65bf1af5ad6fb63a08d408b610ac2ff
VuthyVey <vuthy.v@ligercambodia.org>
2017-04-08 10:44:11 +0700

    hello

--- client/views/newRecipe.js
+++ client/views/newRecipe.js
@@ -5,7 +5,7 @@ var newRecipe = new ReactiveObj({
 	serve: 0,
 	duration: 0,
 	level: "",
-	ingredients: [],
+	ingredients: [{id: 121, name: "bannana", amount: "1", unit: "kg"}],
 	directions: [],
 	owner: ""
 });
@@ -27,10 +27,10 @@ Template.newRecipe.events({
 		var updater = function() {return kh_name};
 		newRecipe.update('kh_name', updater);
 	},
-	//when newIngredient button clicked or keyup, all of the values from inputs will update to newRecipe Obj
+	//when newIngredient button clicked or keyup, all of the values from inputs will add to newRecipe Obj
 	'click #newIngredient, keyup .ingredientsInputs': function(e,tpl) {
 		//if the event is click or Enter key keyup then the code is fire
-		if(e.type == 'click' || (e.type == 'keyup' && e.keyCode == 13)){
+		if(e.type == 'click' || e.keyCode == 13){
 			var name = $('#ingredientName');
 			var amount = $('#ingredientAmount');
 			var unit = $('#ingredientUnit');
@@ -61,32 +61,47 @@ Template.newRecipe.events({
 		//newRecipe.splice("ingredients", id-1, 1);			
 	},
 	'click #editItem': function(e, tpl) {
-		console.log(e)
 		var id = this.id;
-		var ingredients = newRecipe.get("ingredients");
-		
-		var parentElement = e.target.parentElement;
-		parentElement.style.display = "none";
-		parentElement.nextElementSibling.style.display = "initial"
-		console.log(parentElement); 
-
-		for(var i = 0; i < ingredients.length; i++) {
-			if (ingredients[i].id == id) {
-				console.log(newRecipe.get("ingredients." + i));
-				break;
-			}
-		}
-		//newRecipe.splice("ingredients", id-1, 1);			
+		$('.listDiv').show();
+		$('.inputDiv').hide();
+		$('#item'+id).hide();
+		$('#edit'+id).show();
+			
 	},
-	'click #editDone' : function (e, tpl) {
-		var editName = $('#editIngredientName');
-		var editNmount = $('#editIngredientAmount');
-		var editUnit = $('#editIngredientUnit');
+	
+	'click #doneEditBtn, keyup .editInput' : function (e, tpl) {
+		if(e.type == "click" || e.keyCode == 13) {
+			var id = this.id;
+			var name = $('#edit'+id).children('#editedName');
+			var amount = $('#edit'+id).children('#editedAmount');
+			var unit = $('#edit'+id).children('#editedUnit');
 
+			var ingredientsArray = newRecipe.get("ingredients");
+			var ingredientsObj = {id: id, "name": name.val(), "amount": amount.val(), "unit": unit.val()};
+			for(var i = 0; i < ingredientsArray.length; i++) {
+				if (ingredientsArray[i].id == id) {
+					ingredientsArray[i] = ingredientsObj;
+					break;
+				}
+			}
+
+			function updater() {
+				return ingredientsArray
+			}
+			newRecipe.forceInvalidate("ingredients");
+			newRecipe.update("ingredients", updater);
 		
-
-		//Random.id() generates random numbers id
-		//id need to modify or delete items
+			$('#edit'+id).hide();
+			$('#item'+id).show();
+		}
+	},
+	'click #cancelEditBtn' : function (e, tpl) {
+		var id = this.id;
+		$('#edit'+id).hide();
+		$('#item'+id).show();
+		$('#edit'+id).children('#editedName').val(this.name);
+		$('#edit'+id).children('#editedAmount').val(this.amount);
+		$('#edit'+id).children('#editedUnit').val(this.unit);
 	},
 	'click #newDirection, keyup #direction': function(e,tpl) {
 		if(e.type == 'click' || e.keyCode == 13) {
--- client/views/vuthy.js
+++ client/views/vuthy.js
@@ -1,6 +1,6 @@
-// Template.home.helpers({
-// 	//return all of the items from database
-// 	items: function(){
-// 		return Items.find({})
-// 	}
-// });
+Template.home.helpers({
+	//return all of the items from database
+	items: function(){
+		return Items.find({})
+	}
+});
