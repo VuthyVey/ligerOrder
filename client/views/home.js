3f435f5585634dd4901ad8194db89e0d9911c875
VuthyVey <vuthy.v@ligercambodia.org>
2017-05-18 20:29:54 +0700

    The whole design change

--- client/views/home.js
+++ client/views/home.js
@@ -22,6 +22,11 @@ $('.pushpin-demo-nav').each(function() {
     });
   });
 
+ // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
+        
 
         
 }
